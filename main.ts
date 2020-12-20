cb02755954f337a573c8956197d3d0657a61cc8d
Uwe Jugel <uwe.jugel@lovoo.com>
2020-12-20 19:11:24 +0100

    add shields

--- main.ts
+++ main.ts
@@ -1,117 +1,149 @@
 namespace SpriteKind {
     export const Wall = SpriteKind.create()
+    export const Bomb = SpriteKind.create()
 }
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (game.runtime() - bombTime > 1000) {
-        bomb = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . b b b b . . . . . 
-            2 2 2 2 2 2 c b a b a b . . . . 
-            . 5 5 5 2 c c a a a b a b . . . 
-            5 5 2 2 2 c a c a a a b a . . . 
-            5 5 2 2 2 c c a c a a a b . . . 
-            . 5 5 5 2 c a c a c a b a . . . 
-            2 2 2 2 2 2 c a c a c a . . . . 
-            . . . . . . . c c c c . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, spacePlane, 100, 0)
-        bomb.setFlag(SpriteFlag.AutoDestroy, true)
-        music.powerUp.play()
-        bombTime = game.runtime()
-    } else {
-        music.powerDown.play()
-    }
-})
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Wall, function (sprite, otherSprite) {
-    sprite.setVelocity(0, 0)
-    sprite.destroy(effects.fire, 200)
-})
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    dart = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . 5 2 . 2 2 . . . . . . . . . 
-        5 2 . 2 5 2 . . . . . . 2 . . . 
-        . 2 . 2 . 2 . 2 . . . . 2 2 2 . 
-        5 5 5 5 5 5 2 2 2 2 2 2 2 . . . 
-        . 2 . 2 . 2 . 2 . . . . 2 2 2 . 
-        5 2 . 2 5 2 . . . . . . 2 . . . 
-        . . 5 2 . 2 2 . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, spacePlane, 200, 0)
-    dart.setFlag(SpriteFlag.AutoDestroy, true)
-    music.pewPew.play()
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    otherSprite.setVelocity(0, 0)
-    otherSprite.destroy(effects.fire, 100)
-    sprite.destroy(effects.confetti, 100)
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Bomb, function (sprite, otherSprite) {
+    destroySprite(sprite)
     info.changeScoreBy(1)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    otherSprite.setVelocity(0, 0)
-    otherSprite.destroy(effects.fire, 100)
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    shoot(sprites.createProjectileFromSprite(img`
+        . . . . . . . 9 . . . . . . . . 
+        . . . . . . . 6 9 . f . f . f . 
+        . . . . . . . . 6 9 . . . . . . 
+        . . . . . . . . 6 9 . f . f . f 
+        . . . . . . . . 6 9 . . . . . . 
+        . . . . . . . . . 6 9 . f . f . 
+        . . . . . . . . . 6 9 . . . . . 
+        . . . . . . . . . 6 9 f . f . f 
+        . . . . . . . . . 6 9 . . . . . 
+        . . . . . . . . . 6 9 . f . f . 
+        . . . . . . . . 6 9 . . . . . . 
+        . . . . . . . . 6 9 . f . f . f 
+        . . . . . . . . 6 9 . . . . . . 
+        . . . . . . . 6 9 . f . f . f . 
+        . . . . . . . 9 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, ship, 100, 0), _BOMB)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Wall, function (sprite, otherSprite) {
+    destroySprite(sprite)
+})
+function destroySprite (sprite: Sprite) {
+    sprite.setVelocity(0, 0)
+    sprite.destroy(effects.fire, 100)
+    music.baDing.play()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    shoot(sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . 4 4 2 . . . . 
+        . . . . . . . 4 2 4 5 4 2 . . . 
+        . . . . 4 . 4 5 4 5 6 5 4 2 . . 
+        . . 4 . . 4 5 4 5 5 6 6 5 2 . . 
+        . . . . 4 . 4 5 4 5 6 5 4 2 . . 
+        . . . . . . . 4 2 4 5 4 2 . . . 
+        . . . . . . . . . 4 4 2 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, ship, 200, 0), _DART)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    destroy(sprite, otherSprite)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    destroySprite(sprite)
     info.changeLifeBy(-1)
 })
+function shoot (shot: Sprite, _type: number) {
+    shot.setFlag(SpriteFlag.AutoDestroy, true)
+    if (_type == _DART) {
+        shot.setKind(SpriteKind.Projectile)
+        music.pewPew.play()
+    } else {
+        if (_type == _BOMB && game.runtime() - bombTime > 1000) {
+            shot.setKind(SpriteKind.Bomb)
+            bombTime = game.runtime()
+            music.jumpUp.play()
+        } else {
+            shot.destroy()
+        }
+    }
+}
+function destroy (projectile: Sprite, sprite: Sprite) {
+    destroySprite(projectile)
+    destroySprite(sprite)
+}
 let bogey: Sprite = null
-let dart: Sprite = null
-let bomb: Sprite = null
-let spacePlane: Sprite = null
+let ship: Sprite = null
 let bombTime = 0
+let _DART = 0
+let _BOMB = 0
+music.setVolume(20)
+_BOMB = 0
+_DART = 1
+let _BOMB_COOLDOWN = 1000
 bombTime = 0
-spacePlane = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . 9 9 . . . . 
-    . . . b b b b b b 6 6 6 9 . . . 
-    5 4 c c c c c c c b 6 6 6 b . . 
-    5 4 c b b b b b b b b b b c . . 
-    . . . b c c c c b c c c c . . . 
-    . . . . b b b b . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+ship = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........99............
+    ...bbbbbb6669...........
+    54cccccccb666b..........
+    54cbbbbbbbbbbc..........
+    ...bccccbcccc...........
+    ....bbbb................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
     `, SpriteKind.Player)
 let wall = sprites.create(img`
-    . . . . . . c c c c . . . . . . 
-    . . . . . . c b b c . . . . . . 
-    . . . . . . c c b c . . . . . . 
-    . . . . . . c b b c . . . . . . 
-    . . . . . . c b c c . . . . . . 
-    . . . . . . c b b c . . . . . . 
-    . . . . . . c c b c . . . . . . 
-    . . . . . . c b b c . . . . . . 
-    . . . . . . c b c c . . . . . . 
-    . . . . . . c b b c . . . . . . 
-    . . . . . . c c b c . . . . . . 
-    . . . . . . c b b c . . . . . . 
-    . . . . . . c b c c . . . . . . 
-    . . . . . . c b b c . . . . . . 
-    . . . . . . c c b c . . . . . . 
-    . . . . . . c c c c . . . . . . 
+    ................9.......
+    ................69......
+    .................69.....
+    ..................69....
+    ..................69....
+    ...................69...
+    ...................69...
+    ...................69...
+    ....................69..
+    ....................69..
+    ....................69..
+    ....................69..
+    ....................69..
+    ....................69..
+    ....................69..
+    ....................69..
+    ...................69...
+    ...................69...
+    ...................69...
+    ..................69....
+    ..................69....
+    .................69.....
+    ................69......
+    ................9.......
     `, SpriteKind.Wall)
-spacePlane.setFlag(SpriteFlag.StayInScreen, true)
+ship.setFlag(SpriteFlag.StayInScreen, true)
 wall.setPosition(30, 50)
-spacePlane.setPosition(10, 50)
+ship.setPosition(10, 50)
+wall.follow(ship, 30)
 info.setLife(3)
-controller.moveSprite(spacePlane, 200, 200)
+controller.moveSprite(ship, 200, 200)
+forever(function () {
+    pause(100)
+})
 game.onUpdateInterval(500, function () {
     bogey = sprites.create(img`
         . . . . . . . . . . . . . . . . 
